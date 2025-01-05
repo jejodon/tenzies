2a15136d4981380af3dc5af1d5f566579a41c51a
JesseJojo <jessedonkor21@gmail.com>
2025-01-05 14:36:56 +0000

    made die face responsive and the roll dice btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import React from "react"
 import Die from "./Die"
 
 export default function App() {
+  const [dice, setDice] = React.useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    const randomNumbers = []
+
+    for (let i = 0; i < 10; i++) {
+      let result = Math.ceil(Math.random() * 6)
+      randomNumbers.push(result)
+    }
+    return randomNumbers
+  }
+
+  function rollAgain() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map((number) => (<Die value={number} />))
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />      
-        <Die value={2} />      
-        <Die value={3} />      
-        <Die value={4} />      
-        <Die value={5} />      
-        <Die value={6} />      
-        <Die value={1} />      
-        <Die value={2} />      
-        <Die value={3} />      
-        <Die value={5} />
-      </div>      
+        {diceElements}
+      </div>
+
+      <button className="roll-dice" onClick={rollAgain}>Roll</button>      
     </main>
   )
 }
